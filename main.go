aff81a6f98b90bc52d1e5cd0fa7fb803da2a680d
CryptoLeek <malik.jackson@sedsolutions.org>
2023-04-28 10:03:43 -0400

    added id query string param to post handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -36,12 +38,17 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 // Add snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.Header().Set("Allow", http.MethodGet)
-		http.Error(w, "Method not Allowed", 405)
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Display an specific snippet"))
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func main() {
